7a65ba9caa08e02ad421469452da6990cf18e595
akash-git-hub <rathoreakash8965@gmail.com>
2024-09-03 11:08:20 +0530

    1st Commit

--- src/components/ProjectDetailIcon.js
+++ src/components/ProjectDetailIcon.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Image, Stack } from 'react-bootstrap'
+import {  Image, Stack } from 'react-bootstrap'
 import styled from 'styled-components'
 
 const Textp = styled.p`
